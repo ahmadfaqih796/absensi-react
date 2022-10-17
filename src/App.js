a2e5f2d5fb9f37786a34a0edc9b7e1398245854d
Indracjy18 <97542864+Indracjy18@users.noreply.github.com>
2022-10-18 01:52:25 +0700

    create menu login admin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import LoginAdmin from "./pages/admin/Login";
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
-      <h2>makan qub</h2>
-      <h3>belum makan lapar nih, laper</h3>
+      <LoginAdmin />
     </>
   );
 }
---
+++ src/pages/admin/Login.js
@@ -0,0 +1,21 @@
+function LoginAdmin(params) {
+  return (
+    <main className="tampilan">
+      <section className="card">
+        <img src={Image} alt="adminlogo" />
+        <form action="">
+          <div className="grup">
+            <label htmlFor="">Username</label>
+            <input type="text" />
+          </div>
+          <div className="grup">
+            <label htmlFor="">Password</label>
+            <input type="password" />
+          </div>
+        </form>
+      </section>
+    </main>
+  );
+}
+
+export default LoginAdmin;
