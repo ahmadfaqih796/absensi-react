be4713dc66a00889b9ddb72a901b573c5798683c
yaqub29 <112183963+yaqub29@users.noreply.github.com>
2022-10-19 00:38:51 +0700

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "../styles/Navbar.css";
+import absensi from "../assets/image/absensi.png"
+
+
+
+export default function Navbar() {
+  return (
+    <nav className="navigation" >
+        <a href="/" className="absensi" > ABSENSI </a>
+      <button className="menu">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className="navigation-menu">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
